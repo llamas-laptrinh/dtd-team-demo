ed6b4d2b66817a2ed1934018bbccee51d23daf7c
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2024-07-20 23:00:29 +0700

    push code bookhotel, tour

--- booking-tour-girl/src/routes/AppRoutes.tsx
+++ booking-tour-girl/src/routes/AppRoutes.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import MailVerificationResult from "../pages/Auth/MailVerificationResult";
 import Intro from "../pages/Intro/Intro";
 import Layout from "../pages/Layout";
+import Auth from "../pages/Authen";
+import ForgotPassword from "../pages/ForgotPassword";
+import BookingHotel from "../pages/BookingHotel";
+import BookCombo from "../pages/BookCombo";
+import DetailTour from "../pages/DetailTour";
 
 export const AppRouter = () => {
     return (
@@ -9,6 +14,11 @@ export const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route path='/Intro' element={<Intro />} />
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/bookinghotel" element={<BookingHotel />} />
+                    <Route path="/bookcombo" element={<BookCombo />} />
+                    <Route path="/detailtour" element={<DetailTour />} />
                 </Route>
             </Routes>
         </BrowserRouter >
