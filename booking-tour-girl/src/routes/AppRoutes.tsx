7e515222ef304546b583faba998b066c127590be
tuhoccc <tamnguyen22423@gmail.com>
2024-07-23 20:30:55 +0700

    bookticket

--- booking-tour-girl/src/routes/AppRoutes.tsx
+++ booking-tour-girl/src/routes/AppRoutes.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import MailVerificationResult from "../pages/Auth/MailVerificationResult";
 import Intro from "../pages/Intro/Intro";
 import Layout from "../pages/Layout";
-
+import BookTickets from "../pages/bookTicket/BookTickets";
+import ChuyenDi from "../pages/bookTicket/ChuyenDi";
+import Booking from "../pages/bookTicket/Booking";
 export const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path='/Intro' element={<Intro />} />
-                </Route>
-            </Routes>
-        </BrowserRouter >
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/Intro" element={<Intro />} />
+          <Route path="/bookTicket" element={<BookTickets />} />
+          <Route path="/Chuyen-Di" element={<ChuyenDi />} />
+          <Route path="/booking" element={<Booking />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
