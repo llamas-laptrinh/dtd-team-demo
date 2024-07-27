1f8efbbf4abf8a99bceb03ef7d2e22b8f2179aa2
Tran Thanh Nguyen <trannguyen.06042002@gmail.comn>
2024-07-27 21:38:09 +0700

    nguyen api user and authen

--- BookingTourGirl/app/Http/Controllers/Auth/RegisteredUserController.php
+++ BookingTourGirl/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
@@ -10,6 +11,7 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use App\Classes\ApiResponseClass;
 
 class RegisteredUserController extends Controller
 {
@@ -18,24 +20,29 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
-    {
+    public function store(Request $request)
+    {   
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
+        DB::beginTransaction();
+        try{
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->string('password')),
+            ]);
+            $token = $user->createToken("myToken")->plainTextToken;
+            DB::commit();
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->string('password')),
-        ]);
-
-        event(new Registered($user));
-
-        Auth::login($user);
-
-        return response()->noContent();
+            event(new Registered($user));
+            // Auth::login($user);
+            return ApiResponseClass::sendResponse(['user'=>$user,'token'=>$token],'',200);
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
+
 }
--- BookingTourGirl/routes/api.php
+++ BookingTourGirl/routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\LoginUserController;
+use App\Http\Controllers\Auth\UserProfileController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('/register', [RegisteredUserController::class,'store']); 
+Route::post('/login', [LoginUserController::class,'login']); 
+
+Route::group(['middleware'=> ["auth:sanctum"]], function(){
+        Route::post('updateProfile', [UserProfileController::class,'updateProfile']); 
+        Route::put('updatePassword', [UserProfileController::class,'updatePassword']); 
+        Route::get('logout', [LoginUserController::class,'logout']); 
 });
