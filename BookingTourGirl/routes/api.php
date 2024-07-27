6ac24d3fae59f5b1d61fd3145bd9a79aa5ce8749
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2024-07-27 19:21:25 +0700

    created

--- BookingTourGirl/routes/api.php
+++ BookingTourGirl/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TourController;
 use App\Http\Controllers\Api\RelatedTourController;
+use App\Http\Controllers\Api\OrderController;
 
 
 Route::apiResource('tours', TourController::class);
 Route::apiResource('related-tours', RelatedTourController::class);
+Route::apiResource('orders', OrderController::class);
